c7b0dfa909dc201cf1753e91f9ce7c5af4eb3860
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-08-02 11:01:21 -0700

    jhgfjhgfkkkkgjhg

--- TheConversationsBot/Domain/Models/Message.cs
+++ TheConversationsBot/Domain/Models/Message.cs
@@ -4,11 +4,6 @@ namespace TheConversationsBot.Domain.Models;
 
 public class Message
 {
-    //from_id long
-    //message object
-      //  chat_id long
-       // type message_type
-       // board_id long
        public long FromId { get; set; }
        public object Messages { get; set; }
        public long ChatId { get; set; }
