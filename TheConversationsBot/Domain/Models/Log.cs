60f22f662c8abfccce9f27f950e7a7a82a35a930
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-02 22:56:09 +0500

    creaded

--- TheConversationsBot/Domain/Models/Log.cs
+++ TheConversationsBot/Domain/Models/Log.cs
@@ -2,5 +2,9 @@
 
 public class Log
 {
-    
+    public DateTime Date { get; set; }
+    public long FromId { get; set; }
+    public long ToId { get; set; }
+    public string Action { get; set; }
+    public string ExceptionMessage { get; set; }
 }
