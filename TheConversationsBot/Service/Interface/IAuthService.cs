eab6723bcbd7011d971742334c58be0b0063e945
mamat <mamatov.nodir0506@mail.com>
2023-08-03 21:35:56 +0500

    o`zgartirildi

--- TheConversationsBot/Service/Interface/IAuthService.cs
+++ TheConversationsBot/Service/Interface/IAuthService.cs
@@ -2,14 +2,7 @@ namespace TheConversationsBot.Service.Interface;
 
 public interface IAuthService
 {
-    Task<bool> CheckClientAsync(long TelegramClientID);
-    Task<bool> CheckClientRegistrationAsync(long TelegramClientID);
-    Task<bool> RegistrationNumber(long Id, string Number);
-    Task<bool> RegistrationPassword(string Password);
-    Task<bool> RegistrationNickname(String Nicknam);
-    Task<bool> CheckPasswordAsync(string Password);
     Task<bool> CheckNickNameAsync(string NickName);
     Task<bool> CheskPhoneNumber(string Number);
-
-
-}   
+    Task<bool> CheckPasswordAsync(string Password);
+}
--- TheConversationsBot/Service/Interface/IConversation.cs
+++ TheConversationsBot/Service/Interface/IConversation.cs
@@ -6,25 +6,25 @@ namespace TheConversationsBot.Service.Interface;
 
 public interface IConversation
 {
-    Task StartNewConversationAsync(long TelegramChatID);
+   
 
-    Task LastConversationAsync(long TelegramClientId);
-    Task<bool> CheckConversationLimit(long TelegramClientId);
+   
+    
 
 
-    Task CreateRoom((long,long)RoomForCouple);
-    Task TelegramChatID(long TelegramID);
-
+ 
 
 
 
 
-    Task<bool> CheckAviableClient(List<Client> clients);
 
-    Task DelateFromRoomStartConversation((long, long) CleaningRoom);
-    Task AddClientsToRoom(long TelegramClientId);
 
-    Task<bool> StopCommand(long TelegramClientChatId);
+  
 
-    Task DelateConversationFromRoom(long TelegramChatId);
+ 
+  
+
+  
+
+  
 }
