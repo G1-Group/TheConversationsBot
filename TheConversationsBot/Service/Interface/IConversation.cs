d023b80c33d3f3a2e49157696e961464f329c816
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-08-03 07:12:26 -0700

    41-topshiriq bajarildi

--- TheConversationsBot/Service/Interface/IConversation.cs
+++ TheConversationsBot/Service/Interface/IConversation.cs
@@ -1,7 +1,10 @@
+using TheConversationsBot.Domain.Models;
+
 namespace TheConversationsBot.Service.Interface;
 
 public interface IConversation
 {
     Task LastConversationAsync(long TelegramClientId);
     Task<bool> CheckConversationLimit(long TelegramClientId);
+    Task<bool> CheckAviableClient(List<Client> clients);
 }
