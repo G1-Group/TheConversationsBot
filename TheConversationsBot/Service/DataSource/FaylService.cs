4bcc80cd8d6da6adc16692ba7f5a99898d057210
mamat <mamatov.nodir0506@mail.com>
2023-08-03 22:35:38 +0500

    .

--- TheConversationsBot/Service/DataSource/FaylService.cs
+++ TheConversationsBot/Service/DataSource/FaylService.cs
@@ -1,6 +1,9 @@
+using System.Text.Json;
+
 namespace TheConversationsBot.Service.Interface;
 
-public partial class FaylService
+public class FaylService
 {
-    
+    private T Deserialize<T>(string data) => JsonSerializer.Deserialize<T>(data);
+    private string Serialize(object data) => JsonSerializer.Serialize(data);
 }
