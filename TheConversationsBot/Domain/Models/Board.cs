a80e1dae1d1e7cf6c85a728940ca7a67435356f5
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-08-03 04:51:53 -0700

    metod lar yaratildi

--- TheConversationsBot/Domain/Models/Board.cs
+++ TheConversationsBot/Domain/Models/Board.cs
@@ -2,9 +2,7 @@ namespace TheConversationsBot.Domain.Models;
 
 public class Board
 {
-   // board_id long
-       // nickname string
-       // owner_id long
+ 
     public long BoardId  { get; set; }
     public string Nickname { get; set; }
     public long OwnerId { get; set; }
