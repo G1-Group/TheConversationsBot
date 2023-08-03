0d9c051dfe36f71c6af5f47614fc440ee9afbb75
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2023-08-03 19:12:44 +0500

    CreateRoom is added

--- TheConversationsBot/Service/Interface/IConversation.cs
+++ TheConversationsBot/Service/Interface/IConversation.cs
@@ -1,7 +1,10 @@
+using System.Reflection.Emit;
+
 namespace TheConversationsBot.Service.Interface;
 
 public interface IConversation
 {
     Task LastConversationAsync(long TelegramClientId);
     Task<bool> CheckConversationLimit(long TelegramClientId);
+    Task CreateRoom((long,long)RoomForCouple);
 }
