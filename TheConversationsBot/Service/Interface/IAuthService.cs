96c2bdd3abb2bb10ebf37db9aea612762d5aafcf
mamat <mamatov.nodir0506@mail.com>
2023-08-03 17:07:36 +0500

    Bu tasklar bajarildi

--- TheConversationsBot/Service/Interface/IAuthService.cs
+++ TheConversationsBot/Service/Interface/IAuthService.cs
@@ -5,5 +5,9 @@ public interface IAuthService
     Task<bool> CheckClient(long TelegramClientID);
 
     Task<bool> CheckClientRegistration(long TelegramClientID);
+    Task<bool> RegistrationNumber(long Id, string Number);
+    Task<bool> RegistrationPassword(string Password);
+    Task<bool> RegistrationNickname(String Nicknam);
+
 
 }
