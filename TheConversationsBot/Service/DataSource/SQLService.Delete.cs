766d82073811e7f738381ad0a5996ed76174e104
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-05 18:03:21 +0500

    Sql servcice Delete metod yaratildi

---
+++ TheConversationsBot/Service/DataSource/SQLService.Delete.cs
@@ -0,0 +1,26 @@
+﻿using TheConversationsBot.Domain.Models;
+
+namespace TheConversationsBot.Service.Interface;
+
+public partial class SQLService
+{
+    public async Task DeleteUserAsync(User user)
+    {
+    }
+
+    public async Task DeleteClientAsync(Client client)
+    {
+    }
+
+    public async Task DeleteMessageAsync(Message message)
+    {
+    }
+
+    public async Task DeleteAnonymChatAsync(Client client)
+    {
+    }
+
+    private async Task DeleteBoardAsync(Client client)
+    {
+    }
+}
