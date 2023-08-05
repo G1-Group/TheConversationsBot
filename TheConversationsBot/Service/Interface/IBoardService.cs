76743fb627b24fb67ad476d92e097933153ee0d9
AbduRohim2004 <abdurohimbaxtiyorov@gmail.com>
2023-08-05 18:13:41 +0500

    SendMessage is added

--- TheConversationsBot/Service/Interface/IBoardService.cs
+++ TheConversationsBot/Service/Interface/IBoardService.cs
@@ -5,4 +5,5 @@ namespace TheConversationsBot.Service.Interface;
 public interface IBoardService
 {
     Task<Board> CreateBoard(Client client, string nickname);
+    Task<Message> SendMessage(long boardId, string message);
 }
