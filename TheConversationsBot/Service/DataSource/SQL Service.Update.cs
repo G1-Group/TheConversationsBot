91c49928dc1ffba3e567d2374bbc274618cdc613
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-08-05 00:49:43 -0700

    68-Update bajarildi

---
+++ TheConversationsBot/Service/DataSource/SQL Service.Update.cs
@@ -0,0 +1,36 @@
+using TheConversationsBot.Domain.Models;
+
+namespace TheConversationsBot.Service.Interface;
+
+public partial class SQLService
+{
+    public async Task UpdateUsers(List<User> users)
+    {
+        
+    }
+
+    public async Task UpdateUser(User user)
+    {
+        
+    }
+
+    public async Task UpdateClient(Client client)
+    {
+        
+    }
+
+    public async Task UpdateMessage(Message message)
+    {
+        
+    }
+
+    public async Task UpdateAnonymChat(Client client)
+    {
+        
+    }
+
+    private async Task UpdateBoard(Client client)
+    {
+        
+    }
+    
