c141c4321382f7477f9e447bdc7df9d0118fbdd4
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2023-08-05 21:08:04 +0500

    chala

--- TheConversationsBot/TelegramBot/TelegramBot.cs
+++ TheConversationsBot/TelegramBot/TelegramBot.cs
@@ -1,6 +1,26 @@
+using Telegram.Bot;
+
 namespace TheConversationsBot.TelegramBot;
 
 public class TelegramBot
 {
-    
+    private string token = "";
+    private TelegramBotClient botClient;
+
+    public TelegramBot()
+    {
+        
+    }
+
+    private void Initialize()
+    {
+        this.botClient = new TelegramBotClient(token);
+
+        SessionManager sessionManager = new SessionManager(null);
+
+        ControllerManager controllerManager = new ControllerManager(botClient);
+
+        this.botClient.StartReceiving(controllerManager);
+    }
+
 }
