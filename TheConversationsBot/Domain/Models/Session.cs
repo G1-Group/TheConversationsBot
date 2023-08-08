d5397e6831075fe7ad632c8f962ec9eb60f0965c
mamat <mamatov.nodir0506@mail.com>
2023-08-08 11:26:00 +0500

    Session has created

---
+++ TheConversationsBot/Domain/Models/Session.cs
@@ -0,0 +1,12 @@
+namespace TheConversationsBot.Domain.Models;
+
+public class Session
+{
+    public long Id { get; set; }
+    public User User { get; set; }
+    public string Action { get; set; }
+    public string Controller { get; set; }
+    public long ChatId { get; set; }
+    public string UserLogin { get; set; }
+    public string UserPassword { get; set; }
+}
