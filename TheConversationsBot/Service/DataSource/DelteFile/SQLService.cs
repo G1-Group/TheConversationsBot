269565609ec2b5048fa1e00ea93bb041e9115355
mamat <mamatov.nodir0506@mail.com>
2023-08-08 17:01:14 +0500

    delete sqlservice files

--- TheConversationsBot/Service/DataSource/DelteFile/SQLService.cs
+++
@@ -1,9 +0,0 @@
-namespace TheConversationsBot.Service.DataSource;
-
-public partial class SQLService : DataProvider
-{
-    public SQLService(string connectionString) : base(connectionString)
-    {
-        
-    }
-}
