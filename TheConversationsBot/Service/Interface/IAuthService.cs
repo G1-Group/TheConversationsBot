cbc08ce4dbe6bdb2a477c525a72b9b7350f9a6a7
mamat <mamatov.nodir0506@mail.com>
2023-08-09 14:46:16 +0500

    sozlash

--- TheConversationsBot/Service/Interface/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using TheConversationsBot.Domain.Models;
-
-namespace TheConversationsBot.Service.Interface;
-
-public interface IAuthService
-{
-    Task<User> Login(string phoneNumber, string password);
-    public Task<Client> Registration(User user);
-}
