d40b16f5da2cba1815a123b7d9522499791045c2
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-08-16 20:20:20 +0500

    created AuthController

--- TheConversationsBot/TelegramBot/Managers/ManagerController.cs
+++ TheConversationsBot/TelegramBot/Managers/ManagerController.cs
@@ -1,13 +1,27 @@
 using TheConversationsBot.Domain.Models;
+using TheConversationsBot.Service;
+using TheConversationsBot.Service.DataSource;
 using TheConversationsBot.TelegramBot.Controllers;
 
 namespace TheConversationsBot.TelegramBot.Managers;
 
 public class ManagerController
 {
-    public ManagerController()
+    private readonly AuthService _authService;
+    private HomeController _homeController;
+    private AuthController _authController;
+    private ClientDashboardController _clientDashboardController;
+    private BoardController _boardController;
+    private ConversationController _conversationController;
+
+    public ManagerController(AuthService authService)
     {
-        
+        _authService = authService;
+        _homeController = new HomeController();
+        _authController = new AuthController(this,authService);
+        _clientDashboardController = new ClientDashboardController();
+        _boardController = new BoardController(this);
+        _conversationController = new ConversationController(this);
     }
 
 
