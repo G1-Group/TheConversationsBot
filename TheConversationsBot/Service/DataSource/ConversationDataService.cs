1b2961ff63392c465381af2ef8b94a224248eec6
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-16 16:24:53 +0500

    ConversationDataService created

---
+++ TheConversationsBot/Service/DataSource/ConversationDataService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheConversationsBot.Domain.Models;
+
+namespace TheConversationsBot.Service.DataSource;
+
+public class ConversationDataService : DataServiceBase<Conversation>,IConversationDataService
+{
+    public ConversationDataService(DbContext context) : base(context)
+    {
+    }
+}
