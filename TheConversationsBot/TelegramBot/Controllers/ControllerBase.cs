6372b8c203bfcc43e4dfe704191054bacb516e5d
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-16 18:58:26 +0500

    Context Extensionts and Keyboard Extensoins created

--- TheConversationsBot/TelegramBot/Controllers/ControllerBase.cs
+++ TheConversationsBot/TelegramBot/Controllers/ControllerBase.cs
@@ -1,20 +1,45 @@
-﻿using TheConversationsBot.TelegramBot.Managers;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TheConversationsBot.TelegramBot.Extensions;
+using TheConversationsBot.TelegramBot.Managers;
 
 namespace TheConversationsBot.TelegramBot.Controllers;
 
 public abstract class ControllerBase
 {
-    private readonly ManagerController _managerController;
+    protected readonly ITelegramBotClient _botClient = TelegramBot.BotClient;
+    protected readonly ManagerController _managerController;
+    protected Message message { get; set; }
 
     public ControllerBase(ManagerController managerController)
     {
         _managerController = managerController;
     }
+
+    protected abstract Task Index(Context context);
     protected abstract Task HandleAction(Context context);
     protected abstract Task HandleUpdate(Context context);
-    
+
     public async Task Handle(Context context)
     {
-      
+        SetUpdateMessage(context);
+        string oldController = context.Session.Controller;
+        await this.HandleUpdate(context);
+        if (oldController != context.Session.Controller)
+            await context.Forward(this._managerController);
+        else
+            await this.HandleAction(context);
+    }
+
+    private void SetUpdateMessage(Context context)
+    {
+        if (context.Update.Type is UpdateType.Message)
+        {
+            this.message = context.Update.Message;
+            return;
+        }
+
+        this.message = null;
     }
 }
