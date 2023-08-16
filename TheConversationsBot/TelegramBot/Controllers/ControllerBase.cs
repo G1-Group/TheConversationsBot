0dd396bd7b9ffbe170290cdd4a3c4efb9cb587ac
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-16 18:21:00 +0500

    implement ControllerBase

--- TheConversationsBot/TelegramBot/Controllers/ControllerBase.cs
+++ TheConversationsBot/TelegramBot/Controllers/ControllerBase.cs
@@ -1,6 +1,20 @@
-﻿namespace TheConversationsBot.TelegramBot.Controllers;
+﻿using TheConversationsBot.TelegramBot.Managers;
 
-public class ControllerBase
+namespace TheConversationsBot.TelegramBot.Controllers;
+
+public abstract class ControllerBase
 {
+    private readonly ManagerController _managerController;
+
+    public ControllerBase(ManagerController managerController)
+    {
+        _managerController = managerController;
+    }
+    protected abstract Task HandleAction(Context context);
+    protected abstract Task HandleUpdate(Context context);
     
+    public async Task Handle(Context context)
+    {
+      
+    }
 }
