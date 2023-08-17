3b307fc7dc9165f21de3057e4d883c61397b9102
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-17 15:13:15 +0500

    Client service created and implement

---
+++ TheConversationsBot/Service/ClientService.cs
@@ -0,0 +1,30 @@
+﻿using TheConversationsBot.Domain.Models;
+using TheConversationsBot.Service.DataSource;
+using TheConversationsBot.Service.Interface;
+
+namespace TheConversationsBot.Service;
+
+public class ClientService : IClientService
+{
+    private readonly IClientDataService _clientDataService;
+
+    public ClientService(IClientDataService clientDataService)
+    {
+        _clientDataService = clientDataService;
+    }
+    public async Task<Client> UpdateClientNickname(long clientId, string nickname)
+    {
+        var client = await _clientDataService.GetByIdAsync(clientId);
+        client.Nickname = nickname;
+
+        return await _clientDataService.UpdateAsync(client);
+    }
+
+    public async Task<Client> UpdateClientUsername(long clientId, string username)
+    {
+        var client = await _clientDataService.GetByIdAsync(clientId);
+        client.UserName = username;
+
+        return await _clientDataService.UpdateAsync(client);
+    }
+}
---
+++ TheConversationsBot/Service/Interface/IClientService.cs
@@ -0,0 +1,10 @@
+﻿using TheConversationsBot.Domain.Models;
+using TheConversationsBot.Service.DataSource;
+
+namespace TheConversationsBot.Service.Interface;
+
+public interface IClientService
+{
+     Task<Client> UpdateClientNickname(long clientId, string nickname);
+    Task<Client> UpdateClientUsername(long clientId, string username);
+}
